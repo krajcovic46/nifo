0609a7b9e5ef2a777f4d010dd1578b79f313aec7
krajcovic46 <stanlezzz@gmail.com>
2016-02-17 22:33:25 +0100

    swap accordion in the left part of the GUI to tableview, change how things are initialized in front-end

--- src/IFO/Main.java
+++ src/IFO/Main.java
@@ -4,28 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
+    private Parent root;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Intelingentý organizátor súborov");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
+        initializeRoot(primaryStage);
     }
 
-    void createButtons() {
-        Button button = new Button("Accept");
+    public void initializeRoot(Stage primaryStage) {
+        try {
+            root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Intelingentý organizátor súborov");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 
     public static void main(String[] args) {
-        launch(args);
         Temp t = new Temp();
         try {
             t.go();
@@ -33,5 +40,6 @@ public class Main extends Application {
         catch (IOException e) {
 
         }
+        launch(args);
     }
 }
