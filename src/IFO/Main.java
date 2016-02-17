2c2634621def2ea57efae1e081f58b712e46b6d3
krajcovic46 <stanlezzz@gmail.com>
2016-02-17 21:42:34 +0100

    change folder structure, 1

--- src/IFO/Main.java
+++ src/IFO/Main.java
@@ -1,10 +1,37 @@
 package IFO;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
-public class Main {
-    public static void main (String[] args) throws IOException {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Intelingentý organizátor súborov");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    void createButtons() {
+        Button button = new Button("Accept");
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
         Temp t = new Temp();
-        t.go();
+        try {
+            t.go();
+        }
+        catch (IOException e) {
+
+        }
     }
 }
