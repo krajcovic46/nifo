7811fb3e25f067d93c218f8b02eda6abf7780a42
krajcovic46 <stanlezzz@gmail.com>
2016-02-22 13:42:49 +0100

    Update FileExtensions.java

--- src/IFO/Extensions/FileExtensions.java
+++ src/IFO/Extensions/FileExtensions.java
@@ -27,6 +27,42 @@ public class FileExtensions {
         )));
         EXTENSIONS_MAP = Collections.unmodifiableMap(aMap);
     }
+    
+    static {
+        Map<String, ArrayList<String>> aMap = new HashMap<>();
+        aMap.put("zip", "Archives");
+        aMap.put("7z", "Archives");
+        aMap.put("rar", "Archives");
+        aMap.put("tar", "Archives");
+        aMap.put("gz", "Archives");
+        
+        aMap.put("docx", "Documents");
+        aMap.put("doc", "Documents");
+        aMap.put("txt", "Documents");
+        aMap.put("rtf", "Documents");
+        aMap.put("odt", "Documents");
+        
+        aMap.put("mp3", "Audio");
+        aMap.put("wav", "Audio");
+        aMap.put("aac", "Audio");
+        aMap.put("wma", "Audio");
+        aMap.put("m4a", "Audio");
+        aMap.put("flac", "Audio");
+        
+        aMap.put("avi", "Video");
+        aMap.put("mp4", "Video");
+        aMap.put("mov", "Video");
+        aMap.put("flv", "Video");
+        aMap.put("mpg", "Video");
+        
+        //"pdf", "xls", "csv", "ini", "html"
+        aMap.put("pdf", "Data");
+        aMap.put("xls", "Data");
+        aMap.put("csv", "Data");
+        aMap.put("ini", "Data");
+        aMap.put("html", "Data");
+        
+    }
 
 
 }
